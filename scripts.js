91f66d94a5843130f1b4abb2518ebf932c8af4e1
GSUKYOMU <002250@gangseo.ac.kr>
2025-10-22 19:04:40 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const headers = document.querySelectorAll('.accordion-header');
+    const headers = document.querySelectorAll('.accordion-header-adjust');
 
     headers.forEach(header => {
         header.addEventListener('click', function() {
-            // 클릭된 헤더의 다음 요소(콘텐츠)를 찾습니다.
             const content = this.nextElementSibling;
             
-            // 현재 헤더의 active 클래스를 토글합니다.
             this.classList.toggle('active');
 
-            // 콘텐츠의 높이를 조절하여 열고 닫습니다.
             if (content.style.maxHeight) {
-                // 이미 열려있다면 닫습니다.
                 content.style.maxHeight = null;
-                content.style.padding = '0 15px'; // 닫힐 때 패딩 제거
+                // padding-top과 padding-bottom을 0으로 설정하여 완전히 닫히게 함
+                content.style.paddingTop = '0';
+                content.style.paddingBottom = '0';
             } else {
-                // 닫혀있다면 엽니다.
+                // 열릴 때 content의 높이를 설정하고 padding을 부여
                 content.style.maxHeight = content.scrollHeight + 'px';
-                content.style.padding = '15px'; // 열릴 때 패딩 추가
+                content.style.paddingTop = '20px';
+                content.style.paddingBottom = '20px';
             }
         });
     });
